0e34fb142cdfa2a668092b4f28a006754cf19060
Jeyrs <jeyrschabu@gmail.com>
2013-04-14 21:16:07 -0700

    updated

---
+++ src/main/java/com/jeyrs/algorithms/PrintStringPermutations.java
@@ -0,0 +1,14 @@
+package com.jeyrs.algorithms;
+
+public class PrintStringPermutations {
+	public static void main(String [] args){
+		printStringPermutations("","ab");
+	}
+	public static void printStringPermutations(String prefix, String str){
+		if(str.length() == 0) System.out.println(prefix);
+		else{
+			for(int i=0; i <str.length(); i++)
+				printStringPermutations(prefix+str.charAt(i), str.substring(0, i) + str.substring(i+1));
+		}
+	}
+}
