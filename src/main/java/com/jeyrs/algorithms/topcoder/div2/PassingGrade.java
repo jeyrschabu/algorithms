24c7d4efe682a791123c78d76e57ce2ddfd986e0
Jeyrs Chabu <jeyrschabu@gmail.com>
2013-12-10 09:10:02 -0800

    updated solutions

--- src/main/java/com/jeyrs/algorithms/topcoder/div2/PassingGrade.java
+++ src/main/java/com/jeyrs/algorithms/topcoder/div2/PassingGrade.java
@@ -26,5 +26,17 @@ public class PassingGrade {
 		return last;
 		
 	}
-
+	public int pointsNeeded0(int[] pointsEarned, int[] pointsPossible, int finalExam) {
+		for(int i=0; i <= finalExam; i++)
+			if(ok(pointsEarned, pointsPossible, finalExam,i))
+				return i;
+		
+		return -1;
+	}
+	boolean ok(int a[], int b[], int c, int d){
+		int num  = d, den =c;
+		for(int x : a) num +=x;
+		for(int x: b) den +=x;
+		return num*100 >= den * 65;
+	}
 }
