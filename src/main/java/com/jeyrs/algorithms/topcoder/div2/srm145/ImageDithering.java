302fba7c54bb20e7470c97eed85876f994c19546
jeyrs <chabuj@interactivenw.com>
2014-05-14 10:35:55 -0700

    updated solutions

---
+++ src/main/java/com/jeyrs/algorithms/topcoder/div2/srm145/ImageDithering.java
@@ -0,0 +1,17 @@
+package com.jeyrs.algorithms.topcoder.div2.srm145;
+
+import java.util.*;
+public class ImageDithering {
+	public int count(String dithered, String[] screen) {
+		int count = 0;
+		for(int i = 0; i < dithered.length(); i++){
+			char d = dithered.charAt(i);
+			for(String s : screen){
+				for(int j = 0; j < s.length(); j++)
+					if(d == s.charAt(j)) count++;
+			}
+		}
+		return count;
+	}
+}
+
